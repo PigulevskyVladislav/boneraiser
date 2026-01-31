331e51d009894add3c892a38003ad189606d29d0
PigulevskyVladislav <vlad.pigulevsky@gmail.com>
2026-01-31 17:58:53 +0300

    IPoolable and IResettable interfaces added

---
+++ Assets/_Project/Code/Core/Interfaces/IResettable.cs
@@ -0,0 +1,7 @@
+namespace _Project.Code.Core.Interfaces
+{
+    public interface IResettable
+    {
+        void Reset();
+    }
+}
---
+++ Assets/_Project/Code/Infrastructure/Pools/IPoolable.cs
@@ -0,0 +1,9 @@
+using _Project.Code.Core.Interfaces;
+
+namespace _Project.Code.Infrastructure.Pools
+{
+    public interface IPoolable : IResettable
+    {
+        
+    }
+}
