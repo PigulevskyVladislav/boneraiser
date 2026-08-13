21df227f85419fe40b20594475e73c5b214bebd7
PigulevskyVladislav <vlad.pigulevsky@gmail.com>
2026-08-13 20:02:49 +0300

    state machine updated

--- Assets/_Project/Code/Core/Patterns/StateMachine/GameStateMachine.cs
+++ Assets/_Project/Code/Core/Patterns/StateMachine/GameStateMachine.cs
@@ -1,10 +1,29 @@
 using _Project.Code.Core.Patterns.StateMachine.States;
+using _Project.Code.Infrastructure.Factories;
+using _Project.Code.Infrastructure.ScriptableObjects.Prefabs;
+using UnityEngine;
 
 namespace _Project.Code.Core.Patterns.StateMachine
 {
     public class GameStateMachine
     {
+        private readonly UnityPrefabFactory _prefabFactory;
+        private readonly CommonData _prefabs;
+        private readonly GameObject _canvas;
         private IState _currentState;
+
+        public GameStateMachine(UnityPrefabFactory prefabFactory, CommonData prefabs, GameObject canvas)
+        {
+            _prefabFactory = prefabFactory;
+            _prefabs = prefabs;
+            _canvas = canvas;
+        }
+
+        public void Run()
+        {
+            _currentState = new MainMenuState(_prefabFactory, _prefabs, _canvas);
+            _currentState.Enter();
+        }
 
         public void ChangeState(IState newState)
         {
