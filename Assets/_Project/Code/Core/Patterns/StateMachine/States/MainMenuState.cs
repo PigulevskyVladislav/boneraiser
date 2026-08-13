00d44ef3057c61a6d97d14f94e4804e4bd17fdb6
PigulevskyVladislav <vlad.pigulevsky@gmail.com>
2026-08-13 20:36:13 +0300

    main menu state moved to corresponding namespace

--- Assets/_Project/Code/Core/Patterns/StateMachine/States/MainMenuState.cs
+++ Assets/_Project/Code/Core/Patterns/StateMachine/States/MainMenuState.cs
@@ -1,30 +1,32 @@
-using _Project.Code.Core.Patterns.StateMachine.States;
 using _Project.Code.Infrastructure.Factories;
 using _Project.Code.Infrastructure.ScriptableObjects.Prefabs;
 using UnityEngine;
 
-public class MainMenuState: IState
+namespace _Project.Code.Core.Patterns.StateMachine.States
 {
-    private readonly UnityPrefabFactory _prefabFactory;
-    private readonly CommonData _prefabs;
-    private readonly GameObject _canvas;
-    private GameObject _mainMenu;
+    public class MainMenuState: IState
+    {
+        private readonly UnityPrefabFactory _prefabFactory;
+        private readonly CommonData _prefabs;
+        private readonly GameObject _canvas;
+        private GameObject _mainMenu;
     
-    public MainMenuState(UnityPrefabFactory prefabFactory, CommonData prefabs, GameObject canvas)
-    {
-        _prefabFactory = prefabFactory;
-        _prefabs = prefabs;
-        _canvas = canvas;
-    }
+        public MainMenuState(UnityPrefabFactory prefabFactory, CommonData prefabs, GameObject canvas)
+        {
+            _prefabFactory = prefabFactory;
+            _prefabs = prefabs;
+            _canvas = canvas;
+        }
 
-    public void Enter()
-    {
-        _mainMenu = _prefabFactory.Spawn(_prefabs.MainMenu);
-        _mainMenu.transform.SetParent(_canvas.transform, false);
-    }
+        public void Enter()
+        {
+            _mainMenu = _prefabFactory.Spawn(_prefabs.MainMenu);
+            _mainMenu.transform.SetParent(_canvas.transform, false);
+        }
 
-    public void Exit()
-    {
-        _prefabFactory.Despawn(_mainMenu);
+        public void Exit()
+        {
+            _prefabFactory.Despawn(_mainMenu);
+        }
     }
 }
